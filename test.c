26bf8eb9d7b51f151d0ad880740fa2d495cce5eb
Osman Batur Ince <osmanbaturince@outlook.com>
2021-05-13 20:07:52 +0300

    some tests are done

--- test.c
+++ test.c
@@ -7,11 +7,12 @@
 int main(int argc, char **argv)
 {
     int ret;
-    int fd;
+    int fd[3];
     int i;
     int c;
     char buffer[1024];
     char buffer2[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+    char buffer3[8] = {9, 10, 11, 12, 13, 14, 15, 16};
     int size;
     char vdiskname[200];
 
@@ -30,34 +31,43 @@ int main(int argc, char **argv)
     }
 
     printf ("creating files\n");
+    sfs_create ("file1.bin");
+    sfs_create ("file2.bin");
     sfs_create ("file3.bin");
 
-    fd = sfs_open("file3.bin", MODE_APPEND);
+    fd[0] = sfs_open("file1.bin", MODE_APPEND);
+    fd[1] = sfs_open("file2.bin", MODE_APPEND);
+    fd[2] = sfs_open("file3.bin", MODE_APPEND);
     int write_count = 0;
     for (i = 0; i < 1000; ++i) {
         memcpy (buffer, buffer2, 8); // just to show memcpy
-        write_count += sfs_append(fd, (void *) buffer, 8);
+        write_count += sfs_append(fd[0], (void *) buffer, 8);
+        memcpy (buffer, buffer3, 8); // just to show memcpy
+        write_count += sfs_append(fd[1], (void *) buffer, 8);
     }
     printf("Write Count: %d\n", write_count);
-    sfs_close(fd);
+    sfs_close(fd[0]);
+    sfs_close(fd[1]);
 
-    fd = sfs_open("file3.bin", MODE_READ);
-    size = sfs_getsize(fd);
+    fd[0] = sfs_open("file1.bin", MODE_READ);
+    fd[1] = sfs_open("file2.bin", MODE_READ);
+    size = sfs_getsize(fd[1]);
     for(int l = 0; l < 1024; ++l){
         buffer[l] = 0;
     }
 
     int buffer_cnt[8] = {0, 0, 0, 0, 0, 0, 0, 0};
     for (i = 0; i < size; ++i) {
-        int read_count = sfs_read (fd, (void *) buffer, 1);
+        int read_count = sfs_read (fd[1], (void *) buffer, 1);
         c = buffer[0];
-        buffer_cnt[c - 1] += 1;
+        buffer_cnt[c - 9] += 1;
         c = c + 1;
     }
 
     for(int k = 0; k < 8; ++k){
-        printf("Buffer_cnt[%d]: %d\n", k + 1, buffer_cnt[k]);
+        printf("Buffer_cnt[%d]: %d\n", k + 9, buffer_cnt[k]);
     }
-    sfs_close (fd);
+    sfs_close (fd[0]);
+    sfs_close (fd[1]);
     ret = sfs_umount();
 }
